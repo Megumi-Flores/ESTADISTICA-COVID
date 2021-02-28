9795452c3db1d3825f0a9678728b29617d6ae30c
Megumi-Flores <megumiflorest@gmail.com>
2021-02-28 13:25:30 -0500

    commit semifianl

--- main.js
+++ main.js
@@ -2,5 +2,29 @@ $(document).ready(function(){
 	var url  = "https://raw.githubusercontent.com/Megumi-Flores/ESTADISTICA-COVID/main/data.json"
 	$.getJSON(url,function(data){
 		console.log(data)
+
+    var confirmed = []
+    $.each(data.newconfirmeds,function(id,obj){
+      confirmed.push(obj.newconfirmed)
+    })
+    console.log(confirmed)
+    confirmed.shift()
+
+    var myChart = document.getElementById("myChart").getContext('2d')
+    var chart = new Chart(myChart, {
+      type: "line",
+      data: {
+        labels: confirmed,
+        datasets: [
+          {
+            label: "New Confirmed Cases",
+            data: newconfirmed,
+            backgroundColor: "#f1c40f",
+            minBarLength: 100,
+          },
+        ]
+      }
+      options:{}
+    })
 	})	
 })
