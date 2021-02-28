418538859180f7382939d7c1175bbf999655d54d
Megumi-Flores <megumiflorest@gmail.com>
2021-02-28 13:07:41 -0500

    json codiv

--- main.js
+++ main.js
@@ -1,32 +1,6 @@
 $(document).ready(function(){
-	var	url = ""
-
+	var url  = "https://raw.githubusercontent.com/Megumi-Flores/ESTADISTICA-COVID/main/data.json"
 	$.getJSON(url,function(data){
 		console.log(data)
-
-		var total_active,total_recovered,total_deaths,totas_confirmed
-
-		total_active = data.statewise[0]total_active
-		total_recovered = data.statewise[0]total_recovered
-		total_deaths = data.statewise[0]total_deaths
-		total_confirmed = data.statewise[0]total_confirmed
+	})	
 })
-
-
-
-[
-  {
-    "active": "Switzerland",
-    "confirmed": "",
-    "deaths": "0",
-    "recovered": "0",
-    "date": "29/04/2020 22:37:46"
-  },
-  {
-    "active": "Switzerland",
-    "confirmed": "",
-    "deaths": "0",
-    "recovered": "0",
-    "date": "29/04/2020 22:37:46"
-  }
-]
