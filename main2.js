2786806d4c7db4f2f8256cd4b5426d0dafdb1c7d
Megumi-Flores <megumiflorest@gmail.com>
2021-02-28 15:39:21 -0500

    terminado

--- main2.js
+++ main2.js
@@ -1,14 +1,14 @@
 
 //almacenamos los datos del json en 2 arrays
 $(document).ready(function(){
-	var url  = "https://raw.githubusercontent.com/Megumi-Flores/ESTADISTICA-COVID/main/data.json"/*en linea*/
+	var url  = "https://raw.githubusercontent.com/Megumi-Flores/ESTADISTICA-COVID/main/fallecidos.json"/*en linea*/
 	$.getJSON(url,function(data){
 		console.log(data)
     var date = []//variable para almacenar fecha
     var confirmed = []//variable para almacenar confirmados
-    $.each(data.newconfirmeds,function(id,obj){//sirve para pasar los datos
-      date.push(obj.date)
-      confirmed.push(obj.newconfirmed)
+    $.each(data.newfallecidos,function(id,obj){//sirve para pasar los datos
+      date.push(obj.datef)
+      confirmed.push(obj.fallecidos)
     })
     console.log(confirmed)//necesario´para confirmar f12
     confirmed.shift()
@@ -22,7 +22,7 @@ $(document).ready(function(){
         labels: date,//ordenado por fecha (eje x)
         datasets:[
           {
-            label: "Nuevos Casos Confirmados",//color/nombre que identifica
+            label: "Casos Fallecidos",//color/nombre que identifica
             data: confirmed,// datos a graficar (eje y)
             backgroundColor: "#f1c40f",
             minBarLength: 100, //longitud de la barra
